412abafb915e04f7748826574bf18ecbe0bb7198
Aditya Galang Adzana <adityagalang80@gmail.com>
2024-06-09 20:57:57 +0700

    Add transfer feature

--- user/Module_user.go
+++ user/Module_user.go
@@ -2,6 +2,7 @@ package user
 
 import (
 	"fmt"
+	"time"
 	utils "tugasbesar/utils"
 	"tugasbesar/vardata"
 )
@@ -41,7 +42,7 @@ func LoginUser() {
 	fmt.Print("Password : ")
 	fmt.Scanf("%s\n", &password)
 	fmt.Print("===============\n")
-	userIndex := getUserIndexByTelp(noTelp)
+	userIndex := vardata.GetUserIndexByTelp(noTelp)
 	var isSame = false
 	var verif = false
 	if userIndex != -1 {
@@ -51,7 +52,7 @@ func LoginUser() {
 
 	if verif && isSame {
 		//login success
-		fmt.Println("SUCCESS")
+		MainMenuUser(noTelp)
 	} else {
 		if !verif {
 			fmt.Print("=> Akun anda belum terverifikasi oleh ADMIN\n")
@@ -60,18 +61,120 @@ func LoginUser() {
 		} else {
 			fmt.Print("=> Akun tidak ditemukan\n")
 		}
-		fmt.Print("=> Tekan enter untuk kembali\n")
-		empty := ""
-		fmt.Scanf("%s\n", &empty)
+		inputTemplateforBack("Tekan enter untuk kembali")
 		PanelUser()
 	}
 }
 
-func getUserIndexByTelp(telp string) int {
-	for i := 0; i < len(vardata.UserData); i++ {
-		if vardata.UserData[i].NoTelp == telp {
-			return i
+func MainMenuUser(telp string) {
+	index := vardata.GetUserIndexByTelp(telp)
+	utils.ClearConsole()
+	fmt.Print("=============================\n")
+	fmt.Printf("=> Selamat datang %s\n", vardata.UserData[index].Nama)
+	fmt.Printf("=> No Telp %s \n", vardata.UserData[index].NoTelp)
+	fmt.Printf("=> Saldo Rp %d\n", vardata.UserData[index].Saldo)
+	fmt.Print("=============================\n")
+	fmt.Printf("=> 1. Riwayat Transaksi\n")
+	fmt.Printf("=> 2. Transfer\n")
+	fmt.Printf("=> 3. Pembayaran\n")
+	fmt.Printf("=> 4. Keluar\n")
+	fmt.Print("=============================\n")
+	fmt.Print("Select Menu : ")
+	fmt.Scanf("%d\n", &menuInput)
+	switch menuInput {
+	case 1:
+		fmt.Println("Riwayat Transaksi")
+
+	case 2:
+		TransferMenu(telp)
+
+	case 3:
+		fmt.Println("Pembayaran")
+
+	case 4:
+		PanelUser()
+
+	default:
+		MainMenuUser(vardata.UserData[index].NoTelp)
+	}
+}
+
+func TransferMenu(telp string) {
+	index := vardata.GetUserIndexByTelp(telp)
+	utils.ClearConsole()
+	fmt.Print("========Transfer Uang========\n")
+	fmt.Print("=============================\n")
+	var targetTelp string
+	fmt.Print("=> No Telp Tujuan : ")
+	fmt.Scanf("%s\n", &targetTelp)
+	fmt.Print("===============\n")
+	targetIndex := vardata.GetUserIndexByTelp(targetTelp)
+
+	//target not found
+	if targetIndex == -1 {
+		inputTemplateforBack("No telp tidak ditemukan, Enter untuk kembali ke menu")
+		MainMenuUser(telp)
+	}
+	if telp == targetTelp {
+		inputTemplateforBack("Tidak dapat transfer ke akun yang sama, Enter untuk kembali ke menu")
+		MainMenuUser(telp)
+	}
+
+	// success target
+	utils.ClearConsole()
+	fmt.Print("========Transfer Uang========\n")
+	fmt.Printf("=> No Telp Akun Tujuan : %s \n", vardata.UserData[targetIndex].NoTelp)
+	fmt.Print("=============================\n")
+	fmt.Printf("=> Info Saldo Sekarang : Rp %d\n", vardata.UserData[index].Saldo)
+
+	var transferValue int = 0
+	fmt.Print("Nominal Transfer Rp ")
+	fmt.Scanf("%d\n", &transferValue)
+
+	if transferValue == 0 {
+		inputTemplateforBack("Tidak dapat transfer Rp 0, Enter untuk kembali ke menu")
+		MainMenuUser(telp)
+	} else if transferValue > vardata.UserData[index].Saldo {
+		inputTemplateforBack("Nominal Transfer melebihi saldo, Enter untuk kembali ke menu")
+		MainMenuUser(telp)
+	} else {
+		ReConfirmTransferMoney(index, targetIndex, transferValue)
+	}
+}
+
+func ReConfirmTransferMoney(index, targetIndex, transferValue int) {
+	utils.ClearConsole()
+	fmt.Print("========Transfer Uang========\n")
+	fmt.Printf("=> Nama Akun Tujuan : %s \n", vardata.UserData[targetIndex].Nama)
+	fmt.Printf("=> No Telp Akun Tujuan : %s \n", vardata.UserData[targetIndex].NoTelp)
+	fmt.Printf("=> Total Transfer : Rp %d\n", transferValue)
+	fmt.Print("=============================\n")
+	var USERPIN int = 0
+	fmt.Print("Masukan PIN untuk konfirmasi (0 untuk keluar) : ")
+	fmt.Scanf("%d\n", &USERPIN)
+	if USERPIN == 0 {
+		MainMenuUser(vardata.UserData[index].NoTelp)
+	} else {
+		if vardata.UserData[index].PIN == USERPIN {
+			var data vardata.Transaction
+			data.Transaction_type = 1
+			data.Transfer_account_source = vardata.UserData[index].NoTelp
+			data.Transfer_account_target = vardata.UserData[targetIndex].NoTelp
+			data.Nominal = transferValue
+			data.Datetime = time.Now().String()
+
+			vardata.AddNewTransferData(data)
+			inputTemplateforBack("TRANSFER SUCCESS, enter untuk kembali ke menu")
+			MainMenuUser(vardata.UserData[index].NoTelp)
+		} else {
+			inputTemplateforBack("PIN SALAH, kembali ke main menu")
+			MainMenuUser(vardata.UserData[index].NoTelp)
 		}
 	}
-	return -1
+}
+
+func inputTemplateforBack(message string) {
+	fmt.Printf("=> %s\n", message)
+	empty := ""
+	fmt.Scanf("%s\n", &empty)
 }
--- vardata/variable_data.go
+++ vardata/variable_data.go
@@ -5,6 +5,7 @@ import (
 )
 
 const CONST_maxUserData int = 1000
+const CONST_maxTransactionData int = 1000
 
 type User struct {
 	UID      int
@@ -17,11 +18,26 @@ type User struct {
 	IsActive bool
 }
 
+type Transaction struct {
+	UID                     int
+	Transaction_type        int //1 transfer, 2 pembayaran
+	Transfer_account_source string
+	Transfer_account_target string
+	Nominal                 int
+	Datetime                string
+	Payment_id              string
+	Payment_content         string
+}
+
 var UserData [CONST_maxUserData]User
+var TransactionData [CONST_maxTransactionData]Transaction
 
 func InitializeDataVariable() {
 	UserData = utils.LoadData[[CONST_maxUserData]User]("UserData.json")
 	utils.SaveData(UserData, "UserData.json")
+
+	TransactionData = utils.LoadData[[CONST_maxTransactionData]Transaction]("TransactionData.json")
+	utils.SaveData(TransactionData, "TransactionData.json")
 }
 
 func AddNewUserData(data User) {
@@ -33,9 +49,39 @@ func AddNewUserData(data User) {
 	utils.SaveData(UserData, "UserData.json")
 }
 
+func AddNewTransferData(data Transaction) {
+	val := getEmptyTransactionIndex()
+	if val != -1 {
+		data.UID = val + 1
+		TransactionData[val] = data
+		UserData[GetUserIndexByTelp(data.Transfer_account_source)].Saldo -= data.Nominal
+		UserData[GetUserIndexByTelp(data.Transfer_account_target)].Saldo += data.Nominal
+	}
+	utils.SaveData(UserData, "UserData.json")
+	utils.SaveData(TransactionData, "TransactionData.json")
+}
+
+func getEmptyTransactionIndex() int {
+	for i := 0; i < len(TransactionData); i++ {
+		if TransactionData[i].UID == 0 {
+			return i
+		}
+	}
+	return -1
+}
+
 func getEmptyUserDataIndex() int {
 	for i := 0; i < len(UserData); i++ {
 		if UserData[i].UID == 0 {
+			return i
+		}
+	}
+	return -1
+}
+
+func GetUserIndexByTelp(telp string) int {
+	for i := 0; i < len(UserData); i++ {
+		if UserData[i].NoTelp == telp {
 			return i
 		}
 	}
